5bd27df613a715fdefb637c605060909171777dc
T Z <tzatushevskaya@gmail.com>
2024-06-03 18:41:55 +0200

    - Updated MyClass - Added TatyanaClass

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,6 +1,6 @@
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Noroc!");
         System.out.println("Hello Git!");
     }
 }
---
+++ src/TatyanaClass.java
@@ -0,0 +1,5 @@
+public class TatyanaClass {
+    public static void main(String[] args) {
+        System.out.println("My commit to this project.");
+    }
+}
