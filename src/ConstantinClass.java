60c4c2b06da66ab400c0db249888d650adbfaaca
ShuriKenj1 <kgrichuk14@mail.ru>
2024-06-03 20:01:29 +0300

    - Updated ConstantinClass

--- src/ConstantinClass.java
+++ src/ConstantinClass.java
@@ -1,5 +1,5 @@
 public class ConstantinClass {
     public static void main(String[] args) {
-        System.out.println("Sorry for being dumb😢");
+        System.out.println("Sorry for being dumb");
     }
 }
